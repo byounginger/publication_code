f01f27609ec1e496289dd7006683e3b3e706ec46
Brett <b.younginger@gmail.com>
2020-06-03 13:59:17 -0700

    More modifications to final code

--- temporal7.R
+++ temporal7.R
@@ -2,19 +2,22 @@
 ## "Stable coexistence or competitive exclusion? 
 ## Fern endophytes shift our understanding of microbial temporal turnover"
 
-# setwd('/Users/brett/Documents/Temporal_turnover/Informatics')
+## Outline separate components of the project
+## Outline of the separate analyses and plots
+## 
+
+setwd('/Users/brett/Documents/Temporal_turnover/Informatics')
 
 # Load packages
 library(phyloseq) # 1.32.0
 library(vegan) # 2.5-6
-#library(lattice) # 0.20-41
-#library(permute) # 0.9-5
+# library(lattice) # 0.20-41
+# library(permute) # 0.9-5
 library(ggplot2) # 3.3.0
-
-#library(dplyr) # 0.8.5
-#library(reshape2) # 1.4.4
-#library(plyr) # 1.8.6
-#library(dunn.test) # 1.3.5
+library(dplyr) # 0.8.5
+library(reshape2) # 1.4.4
+library(plyr) # 1.8.6
+library(dunn.test) # 1.3.5
 
 # Figure dimensions for journal (ESA)
 prop = 5/7      
@@ -22,8 +25,8 @@ s_w = 3.00
 s_h = s_w*prop
 h_w = 4.50
 h_h = h_w*prop
-d_w = 6.00   # Consider dropping
-d_h = d_w*prop # Consider dropping
+d_w = 6.00
+d_h = d_w*prop
 
 # Other variables for figure consistency
 # outlier size
@@ -357,6 +360,7 @@ sort(sample_sums(pseq_merge_mocks))
 set.seed(555)
 pseq_mocks_rare <- rarefy_even_depth(pseq_merge_mocks, sample.size = 2996, 
                                      replace = FALSE)
+# Lost 225 OTUs
 
 # Supplemental ordination plot with seq controls
 p3 <- plot_ordination(pseq_mocks_rare, ordinate(pseq_mocks_rare, 'NMDS', 
@@ -395,6 +399,7 @@ colnames(tax_table(wp_pseq)) = c('Kingdom', 'Phylum', 'Class',
 sort(sample_sums(wp_pseq))
 set.seed(555)
 wp_rare <- rarefy_even_depth(wp_pseq, sample.size = 5000, replace = FALSE)
+# Lost 83 OTUs
 
 # Ordination
 p4 <- plot_ordination(wp_rare, 
@@ -434,7 +439,7 @@ hill <- tibble::rownames_to_column(hill, 'SampleID')
 hill <- melt(hill, id.vars = 'SampleID')
 colnames(hill) = c('SampleID', 'Hill_number', 'Hill_value')
 
-# Adjust factor grouping variables in dataset
+# Adjust grouping variables in dataset
 hill$Plant <- as.factor(sub('(\\d+)\\.\\w+', '\\1', hill$SampleID))
 hill$Month <- as.factor(sub('\\d+\\.(\\w+)', '\\1', hill$SampleID))
 hill$Hill_number <- mapvalues(hill$Hill_number, from = c(0,1,2), 
@@ -465,7 +470,6 @@ hillaov <- aov(Hill_value ~ Month + Error(Plant/Month), data = hill2)
 # This is likely because Jul only has 19 individuals
   # will drop this individual from the analysis
 
-summary(hillaov)
 jul_only <- subset(hill2, Month == 'Jul')
 nrow(jul_only)
 jul_only # Plant 6 to remove
@@ -493,27 +497,26 @@ hist(no_six$log_val)
 hillaov3 <- aov(log_val ~ Month + Error(Plant/Month), data = no_six)
 summary(hillaov3)
 
-Tuker <- TukeyHSD(aov(log_val ~ Month, data = no_six), las = 1)
-Tuker$Month
-plot(TukeyHSD(aov(log_val ~ Month, data = no_six)), las = 1)
+Tuk <- TukeyHSD(aov(log_val ~ Month, data = no_six), las = 1)
 
 # Compare with Kruskal
 kruskal.test(Hill_value ~ Month, data = no_six)
 # Same result
 
 ## Capture non-significant post-hoc values for plot annotation
-Tuk2 <- Tuker$Month
+Tuk2 <- Tuk$Month
 Tuk2 <- as.data.frame(Tuk2)
 Tuk2$p_adj <- Tuk2$`p adj`
 non_sig <- subset(Tuk2, p_adj >= 5e-2)
 non_sig
 
-# Alhpa diversity plot with stats annotations
+# Stats annotations
 labels <- c(' A', ' A', ' B', ' C', ' C', ' D', 'D,B', 'D,B', ' D')
 x_locs <- c(1, 2, 3, 4, 5, 6, 7, 8, 9)
 tapply(hill2$Hill_value, hill2$Month, summary)  # To figure out the value of the upper quartile
 y_locs <- c(92.25, 91.50, 11.50, 7.00, 7.75, 27.50, 15.75, 20.75, 24.25)
 
+# Alpha diversity plot
 p7 <- ggplot(hill2, aes(x = Month, y = Hill_value, fill = Month)) + 
   geom_boxplot(outlier.size = outlier) + 
   theme_w_legend() + theme(legend.position = 'none') + ylab('Richness (OTUs)') +
@@ -526,22 +529,11 @@ p7 <- ggplot(hill2, aes(x = Month, y = Hill_value, fill = Month)) +
 
 p7
 
-ggsave('../R/Figures/hill_v5.pdf', width = s_w, height = s_h, units = 'in')
-
-###################
-###################
-###################
-###################
+# ggsave('../R/Figures/hill_v5.pdf', width = s_w, height = s_h, units = 'in')
 
 # Taxonomy plot
-ntaxa(pseq_rare_noApr)
-nsamples(pseq_rare_noApr)
-sample_sums(pseq_rare_noApr)
-20*15000
-
+# Convert to relative abundance
 pseq_rel <- transform_sample_counts(pseq_rare_noApr, function(x) x/3e+05)
-sample_sums(pseq_rel)
-0.05*20
 
 # Supplemental plot without filtering
 sample_data(pseq_rel)$Month <- factor(sample_data(pseq_rel)$Month, 
@@ -558,11 +550,10 @@ p8 + geom_bar(aes(fill = Genus, color = Genus), stat = 'identity',
         axis.ticks.x = element_blank()) + 
   ylab('Relative abundance')
 
-# ggsave('../R/Figures/tax_supp_V1.pdf', width = d_w, height = d_h, units = 'in')
-ggsave('../R/Figures/tax_supp_V2.pdf', width = d_w, height = d_h, units = 'in')
+# ggsave('../R/Figures/tax_supp_V2.pdf', width = d_w, height = d_h, units = 'in')
 
-# MS plot with filtering
-
+# Final plot with filtering
+# Filter low-abundance taxa
 pseq_rel_filt <- filter_taxa(pseq_rel, function(x) mean(x) > 2e-5, prune = TRUE)
 
 p9 <- plot_bar(pseq_rel_filt, x = 'Class', fill = 'Genus') + 
@@ -574,71 +565,29 @@ p9 <- plot_bar(pseq_rel_filt, x = 'Class', fill = 'Genus') +
 
 p9
 
-# ggsave('../R/Figures/tax_V1.pdf', width = d_w, height = d_h, units = 'in')
-ggsave('../R/Figures/tax_V2.pdf', width = h_w, height = h_h, units = 'in')
+# ggsave('../R/Figures/tax_V2.pdf', width = h_w, height = h_h, units = 'in')
 
-## Gametophyte bioassay results
-
+# Gametophyte bioassay results
+# Import data
 gamete <- read.csv('../Gametophytes/Nov_time1.csv', header = T, sep = "\t")
 
+# Adjust data
 gamete$Sample_ID2 <- sub('(\\w+_\\d+)_\\d', '\\1', gamete$SampleID)
 
+# Add taxonomic names to accession numbers
 gamete$SampleID[gamete$Sample_ID2 == 'BSY_163'] <- 'Flagellospora'
 gamete$SampleID[gamete$Sample_ID2 == 'BSY_137'] <- 'Plectania'
 gamete$SampleID[gamete$Sample_ID2 == 'Cont_1'] <- 'Control'
 gamete$SampleID[gamete$Sample_ID2 == 'Cont_2'] <- 'Control'
 
-# p10 <- ggplot(gamete, aes(x=SampleID, y=Nov_Time1, fill = SampleID)) + 
-#   geom_boxplot(outlier.size = 0.5) + facet_grid(~Time_elapsed) + 
-#   theme(panel.background = element_blank(), axis.line = element_line(color = 'black'), 
-#         axis.text = element_text(color = 'black', size = 6, angle = 30, vjust = 0, hjust = 1), 
-#         legend.position = 'none', 
-#         axis.title = element_text(size = 8)) +
-#   geom_hline(yintercept = 0, alpha = 0.5) + xlab('Treatment group') + 
-#   ylab(expression(paste(Delta,' Surface area ', (cm)^2)))
-# 
-# p10
-# 
-# # Need to finish adjusting and adding stats annotations
-# # ggsave('../R/Figures/gamete_V1.pdf', width = s_w, height = s_h, units = 'in')
-# ggsave('../R/Figures/gamete_V2.pdf', width = s_w, height = s_h, units = 'in')
-
-## Stats on the gametophytes
-
+# Stats on affects of fungal inocula on gametophyte growth
 week4 <- subset(gamete, Time_elapsed == '4 weeks')
 week8 <- subset(gamete, Time_elapsed == '8 weeks')
 
-hist(week4$Nov_Time1, breaks = 10)
-shapiro.test(week4$Nov_Time1)
-week4$sq_size <- week4$Nov_Time1^2
-hist(week4$sq_size, breaks = 10)
-week4$log_size <- log(week4$Nov_Time1 + 1)
-hist(week4$log_size, breaks = 20)
-shapiro.test(week4$log_size)
-
-week4$log10 <- log10(week4$Nov_Time1 + 1)
-hist(week4$log10)
-shapiro.test(week4$log10)
-qqnorm(week4$log10)
-qqline(week4$log10)
-
-qqnorm(week4$log_size)
-qqline(week4$log_size)
-
-week4$sqrt <- sqrt(week4$Nov_Time1 + 1)
-hist(week4$sqrt, breaks = 5)
-shapiro.test(week4$sqrt)
-
-hist(week8$Nov_Time1, breaks = 10)
-shapiro.test(week8$Nov_Time1)
-week8$log_size <- log(week8$Nov_Time1 + 1)
-shapiro.test(week8$log_size)
-hist(week8$log_size, breaks = 10)
-qqnorm(week8$log_size)
-qqline(week8$log_size)
-
-## Not likely to meet parametric assumptions. Will need to use the 
-#   Kruskal-Wallis with a correction
+# Not likely to meet parametric assumptions. Will use a 
+  # Kruskal-Wallis rank sum with a Hochberg correction
+  # Analyze each timepoint separately, comparing samples within
+  # a timepoint, rather than as repeated measures
 
 week4_mod1 <- kruskal.test(Nov_Time1 ~ SampleID, data = week4)
 week4_mod1
@@ -654,12 +603,8 @@ f_labels <- data.frame(x = c(1, 1), y = c(-0.25, -0.25),
                        label = c('chi^2(2) = 4.00, p = 0.136', 
                                  'chi^2(2) = 17.11, p < 0.001'))
 
-# 'chi^2(2) = 4.00, p = 0.136'
-# 'chi^2(2) = 17.11, p < 0.001'
-# gamete2 <- dplyr::left_join(gamete, f_labels, by = 'Time_elapsed')
-# 
-# gamete2[31,7] <- 'sample2'
-
+# Impute dummy data into the dataframe to obtain separate annotations
+  # in facets with different statistical results
 gamete2 <- gamete
 gamete2[,5:7] <- NA
 newcols <- c("SampleID", "Nov_Time1", "Time_elapsed", "Sample_ID2", "X", "Y", "label")
@@ -671,8 +616,8 @@ gamete2[31,6] <- -0.25
 gamete2[1,7] <- '4.00, p = 0.136'
 gamete2[31,7] <- '17.11, p < 0.001'
 
-# Plot the post-hoc values:
-tapply(week8$Nov_Time1, week8$SampleID, summary)  # Figure out the value of the upper quartile
+# Figure out the values of the upper quartile to plot the post-hoc values
+tapply(week8$Nov_Time1, week8$SampleID, summary)
 gamete2[32,5] <- 1.1
 gamete2[33,5] <- 2.1
 gamete2[34,5] <- 3.1
@@ -686,9 +631,7 @@ gamete2[32,7] <- 'A'
 gamete2[33,7] <- 'A'
 gamete2[34,7] <- 'B'
 
-# gamete2 <- gamete
-# gamete2[31:60, 2] <- NA
-
+# The plot
 p10 <- ggplot(gamete2, aes(x=SampleID, y=Nov_Time1, fill = SampleID)) + 
   geom_boxplot(outlier.size = outlier) + facet_grid(.~Time_elapsed) +
   theme_w_legend() + 
@@ -704,40 +647,27 @@ p10 <- ggplot(gamete2, aes(x=SampleID, y=Nov_Time1, fill = SampleID)) +
   geom_text(data = gamete2, aes(x = X, y = Y, label = label), 
             hjust = hjust, size = size)
 
-# strip.text = element_text(size = rel(0.5)))
-# Okay, just use cowplot to get the separate plots together. Can I plot
-#   annotations after separately in the facets? Will have to see later. 
-
-# geom_text(data=my.avg, aes(x=1.5, y=7, label=paste("Avg ==", mean, "*m^2/ha")), parse=TRUE)
-# Got this to work, now to remove the overplotting that occurs! 
-# Also reduce facet label size
-# Get the expression values in
-
 p10
 
-#ggsave('../R/Figures/gamete_V2.pdf', width = s_w, height = s_h, units = 'in')
-
-# ggsave('../R/Figures/gamete_V3.pdf', width = s_w, height = s_h, units = 'in')
-
-ggsave('../R/Figures/gamete_V4.pdf', width = s_w, height = s_h, units = 'in')
+# ggsave('../R/Figures/gamete_V4.pdf', width = s_w, height = s_h, units = 'in')
 
 # Competition assay plot
-
+# Load data
 comp1 <- read.csv('../Competition_assays/compiled_images.csv', header = TRUE)
 
-# Should plot the following:
+# EDA of the following:
 # - length:towards
 # - area:towards
 # - area:away
 
-tow_len <- filter(comp1, Direction == 'towards')
-awa_are <- filter(comp1, Direction == 'away')
+tow_len <- dplyr::filter(comp1, Direction == 'towards')
+awa_are <- dplyr::filter(comp1, Direction == 'away')
 
 ggplot(tow_len, aes(x = Competitor, y = Length)) + geom_boxplot()
 ggplot(tow_len, aes(x = Competitor, y = Area)) + geom_boxplot()
 ggplot(awa_are, aes(x = Competitor, y = Area)) + geom_boxplot()
 
-# Will go with the length-towards analysis:
+# Plot the length-towards (amount of growth towards C. polystichicola competitor):
 
 p11 <- ggplot(tow_len, aes(x = Competitor, y = Length, fill = Competitor)) + 
   geom_boxplot(outlier.size = outlier) + theme_w_legend() +
@@ -751,10 +681,4 @@ p11 <- ggplot(tow_len, aes(x = Competitor, y = Length, fill = Competitor)) +
 
 p11
 
-# ggsave('../R/Figures/comp_V1.pdf', width = s_w, height = s_h, units = 'in')
-
-ggsave('../R/Figures/comp_V2.pdf', width = s_w, height = s_h, units = 'in')
-
-
-
-
+# ggsave('../R/Figures/comp_V2.pdf', width = s_w, height = s_h, units = 'in')
